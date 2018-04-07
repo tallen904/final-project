093997453236da6f1e4ab4bd0a1b17f5d4a0a699
unknown <tallen904@gmail.com>
2018-04-07 14:00:07 -0700

    Created dashboard page

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -3,18 +3,20 @@ import './styles.css'
 
 const Navbar = () => {
     return <div className="page-navbar">
-        <div>
-          <a className="logo" href="#!">
-            <h1>RideShare</h1>
-          </a>
+        <div className='nav-content'>
+            <div>
+                <a className="logo" href="#!">
+                    <h1>RideShare</h1>
+                </a>
+            </div>
+            <div>
+                <ul>
+                    <li>Login</li>
+                    <li>Signup</li>
+                </ul>
+            </div>
         </div>
-        <div>
-            <ul>
-                <li>Login</li>
-                <li>Signup</li>
-            </ul>
-        </div>
-      </div>;
+      </div>
 };
 
 export default Navbar;
