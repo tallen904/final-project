ca1436d1f02c5543c50365f6cd27b70ee534ecad
Kevin Castro <kcastro.build@gmail.com>
2018-04-07 14:02:18 -0700

    add in db models, finish 2/3 seeding script

---
+++ models/User.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const newSchema = new Schema({
+  'name': {
+    type      : String,
+    required  : true
+  },
+  'seats': {
+    //seats is potentially optional, if you're just signed up as a rider
+    type      : Number
+  },
+  'email': {
+    //need email validation
+    type      : String,
+    required  : true
+  },
+  'events': [{
+    type      : Schema.Types.ObjectId,
+    ref       : 'Event'
+  }],
+  'createdAt': { type: Date, default: Date.now },
+  'updatedAt': { type: Date, default: Date.now }
+});
+
+//boilerplate for edit flags
+newSchema.pre('save', function(next){
+  this.updatedAt = Date.now();
+  next();
+});
+
+newSchema.pre('update', function() {
+  this.update({}, { $set: { updatedAt: Date.now() } });
+});
+
+newSchema.pre('findOneAndUpdate', function() {
+  this.update({}, { $set: { updatedAt: Date.now() } });
+});
+
+
+
+module.exports = mongoose.model('User', newSchema);
---
+++ models/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  Event: require('./Event'),
+  Car : require('./Car'),
+  User : require('./User')
+}
