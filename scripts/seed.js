8875c54f3cd65315a0c96a5ac5af8e5fba57e532
Kevin Castro <kcastro.build@gmail.com>
2018-04-10 20:16:06 -0700

    reformat some code, change promise chain for seeding

--- scripts/seed.js
+++ scripts/seed.js
@@ -4,43 +4,47 @@ mongoose.Promise = global.Promise;
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/cruzinDb");
 
+//setup dummy data
 const testUser = {
-  name  : 'Johnny Boy',
-  seats  : 4,
-  email  : 'goodboy@johnny.com'
+  name    : 'Johnny Boy',
+  seats   : 4,
+  email   : 'goodboy@johnny.com'
 }
 
-const testCar = (id) => {
-  return {
-    seats   : 4,
-    driver  : id
-  }
-}
+const testCar = (id) => ({
+  seats   : 4,
+  driver  : id
+})
 
-const testEvent => (id) {
-  name     : 'Test Event',
+const testEvent = {
+  name      : 'Test Event',
   time      : Date.now(),
-  location  : '32.852992, -117.182789',
-  // TODO: point to created car db object
-  cars : [id]
+  location  : '32.852992, -117.182789'
 }
 
-//clear out db, then reseed
-db.Event
-  .remove()
+//clear out db state, then reseed
+db.Event.remove({}, () => console.log('Events removed,'))
+  .then(() => db.Car.remove({}, () => console.log('Cars removed,')))
+  .then(() => db.User.remove({}, () => console.log('Users removed.')))
+
+  //create event and user first, will populate event after
   .then(() => db.User.create(testUser))
   .then(dbUser => db.Car.create(testCar(dbUser._id)))
   .then(dbCar => {
-    // TODO: create a event in the db, point it to the car object, have the car point to the event object
-    //create event, set the cars array to point to car that was just created
-    db.Event.create(testEvent(dbCar._id))
+    db.Event.create(testEvent).then(dbEvent => {
+      console.log(dbEvent);
+      //we need to link the car with the event that is created
+      // dbEvent.cars.push(dbCar._id)
+    })
   })
-  .catch(err => throw err)
+
+  //exit messages
   .then(data => {
     console.log('db seeded!')
     process.exit(0)
   })
+  //if any errors occur, throw the err, close out the script
   .catch(err => {
-    console.log(err)
+    throw err;
     process.exit(1)
   })
