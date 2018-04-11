6d462e8e5cda070bc4ea85a708d3e67bde020b18
Kevin Castro <kcastro.build@gmail.com>
2018-04-11 12:02:47 -0700

    add clarifying comments

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 //set up the route to access the db
+//route will look something like /db/MODEL/:id
 router.use('/db', require('./db'))
 
 //export a big router with subrouters
+//right now it's only the api
 module.exports = router
