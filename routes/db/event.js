fd70bd61e33664695fafd41d87b78f75a747d723
Kevin Castro <kcastro.build@gmail.com>
2018-04-12 13:33:01 -0700

    change call to anonymous arrow function returning correct req, res func

--- routes/db/event.js
+++ routes/db/event.js
@@ -5,15 +5,15 @@ const router = express.Router()
 //below will set up routes for posting, getting, putting, and deleting an event object in the db
 //passing through mounted path according to express docs
 //controller will populate the proper function that returns a promise to db
-router.post('/', controller.postDocument(router.path()))
+router.post('/', controller.postDocument())
 
-router.get('/:id', controller.getDocument(router.path()))
+router.get('/:id', controller.getDocument())
 
-router.put('/:id', controller.putDocument(router.path()))
+router.put('/:id', controller.putDocument())
 
-router.delete('/:id', controller.deleteDocument(router.path()))
+router.delete('/:id', controller.deleteDocument())
 
 //get all doc path
-router.get('/all', controller.allDocuments(router.path()))
+router.get('/all', controller.allDocuments())
 
 module.exports = router
--- routes/db/user.js
+++ routes/db/user.js
@@ -5,12 +5,12 @@ const router = express.Router()
 //below will set up routes for posting, getting, putting, and deleting a user object in the db
 //passing through mounted path according to express docs
 //controller will populate the proper function that returns a promise to db
-router.post('/', controller.postDocument(router.path()))
+router.post('/', controller.postDocument())
 
-router.get('/:id', controller.getDocument(router.path()))
+router.get('/:id', controller.getDocument())
 
-router.put('/:id', controller.putDocument(router.path()))
+router.put('/:id', controller.putDocument())
 
-router.delete('/:id', controller.deleteDocument(router.path()))
+router.delete('/:id', controller.deleteDocument())
 
 module.exports = router
