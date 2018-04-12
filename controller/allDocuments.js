bde26d0660b999b3e47d0a789ab5c4e2ac19a968
Kevin Castro <kcastro.build@gmail.com>
2018-04-12 10:29:47 -0700

    add in all Events controller, others pending implementation

---
+++ controller/allDocuments.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const db = require('../models')
+
+//gets all of the docs from the db that meet certain criteria
+module.exports = (path) => {
+  switch (path) {
+    case '/user':
+      return (req, res) => {
+        //TODO:
+        //pending implementation
+        //can get all of the users that have a linked event, but it will likely
+        //be best to get that from the cars that are owned by the event
+      }
+    case '/car':
+      return (req, res) => {
+        //TODO:
+        //pending implementation
+        //should likely send back the list of the cars that are linked to an event
+      }
+    case '/event':
+      return (req, res) => {
+        db.Event.find()
+          .then(events => res.json(events));
+      }
+    default:
+      return (req, res) => {
+        //if the path is not found, send 404
+        res.sendStatus(404)
+      }
+  }
+}
