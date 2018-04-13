1c8f6b2d603e0abdfbfde82120bf76ea48ee3317
Kevin Castro <kcastro.build@gmail.com>
2018-04-12 19:35:02 -0700

    move /all path to first

--- routes/db/event.js
+++ routes/db/event.js
@@ -2,6 +2,9 @@ const controller = require('../../controller');
 const express = require('express');
 const router = express.Router()
 
+//get all doc path
+router.get('/all', controller.allDocuments())
+
 //below will set up routes for posting, getting, putting, and deleting an event object in the db
 //passing through mounted path according to express docs
 //controller will populate the proper function that returns a promise to db
@@ -13,7 +16,5 @@ router.put('/:id', controller.putDocument())
 
 router.delete('/:id', controller.deleteDocument())
 
-//get all doc path
-router.get('/all', controller.allDocuments())
 
 module.exports = router
