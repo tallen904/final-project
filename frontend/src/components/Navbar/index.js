c409d11b58fc7680e053829801f33f119a442758
katkeivens <katkeivens@gmail.com>
2018-04-14 12:27:27 -0700

    fixed styles

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -7,14 +7,14 @@ const Navbar = () => {
             <div className='small-5 columns'>
                 <div>
                     <a className="logo" href="#!">
-                        <img src={logo} alt="logo"/><span>ruzin'</span>
+                        <img src={logo} alt="logo"/><span className='logo-header'>ruzin'</span>
                     </a>
                 </div>
             </div>
             <div className='small-7 columns'>
                 <ul class="menu align-right">
-                    <li><a href="#">Login</a></li>
                     <li><a href="#">Signup</a></li>
+                    <li><a href="#">Login</a></li>
                 </ul>
             </div>
         </div>
