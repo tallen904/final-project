44fa2f7567a7a0d2a924d7ca5f590aa76031e351
Kevin Castro <kcastro.build@gmail.com>
2018-04-17 20:16:03 -0700

    change proxy and server port

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const port = process.env.PORT || 3000;
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const port = process.env.PORT || 3001
 
 
 //body-parser setup
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 //mongoose setup
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/crwsin", err => {
   if (err) throw err;
-  console.log('db connected!');
+  console.log('db connected!')
 });
 
 //routes
 app.use('/', require('./routes'))
 
 app.listen(port, () => {
-  console.log(`🌎  ==> DB now listening on PORT ${port}!`);
+  console.log(`🌎  ==> DB now listening on PORT ${port}!`)
 });
