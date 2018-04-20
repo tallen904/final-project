54f27019bf9894c5452bc23b89d29c86c54d1e37
Kevin Castro <kcastro.build@gmail.com>
2018-04-19 20:06:06 -0700

    add populate routes for user and car models

--- routes/db/car.js
+++ routes/db/car.js
@@ -2,6 +2,9 @@ const controller = require('../../controller');
 const express = require('express');
 const router = express.Router()
 
+//will get all of the populated users (or riders) from a car, given an id
+router.get('/all/:id', controller.allDocuments())
+
 //below will set up routes for posting, getting, putting, and deleting a car object in the db
 //passing through mounted path according to express docs
 //controller will populate the proper function that returns a promise to db
