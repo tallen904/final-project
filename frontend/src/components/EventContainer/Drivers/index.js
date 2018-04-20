82e60347106f6b6ffdcf91decf6a279e29ea5a22
octohorsey <jimmyjen@gmail.com>
2018-04-19 18:26:55 -0700

    test commit

---
+++ frontend/src/components/EventContainer/Drivers/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Drivers = () => {
+	return (
+  <div className='drivers-waitlist events-box-content'>
+   <h1>Drivers Placeholder!</h1>
+  </div>
+
+
+	);
+};
+
+export default Drivers;
---
+++ frontend/src/components/EventContainer/Share/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Share = () => {
+	return (
+  <div className='share-waitlist'>
+   <h1>Share placeholder!</h1>
+  </div>
+
+
+	);
+};
+
+export default Share;
